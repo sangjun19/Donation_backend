a996b97852fb6eb1508cb03906a4b3fd7fd9b8f6
sangjun19 <sangjun0319@gmail.com>
2023-09-19 00:22:04 +0900

    delete firebase

--- src/main/java/com/example/Donation/Config/FirebaseConfig.java
+++ src/main/java/com/example/Donation/Config/FirebaseConfig.java
@@ -1,26 +1,26 @@
-package com.example.Donation.Config;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
-import java.io.IOException;
-
-@Configuration
-public class FirebaseConfig {
-    @PostConstruct
-    public void initialize() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/donation-21c1e-firebase-adminsdk-euymx-8a49dfd3f5.json");// Firebase Admin SDK JSON 파일 경로
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://donation-21c1e-default-rtdb.firebaseio.com") // Firebase Realtime Database URL
-                .build();
-
-        FirebaseApp.initializeApp(options);
-    }
-}
+//package com.example.Donation.Config;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.stereotype.Service;
+//
+//import javax.annotation.PostConstruct;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//
+//@Configuration
+//public class FirebaseConfig {
+//    @PostConstruct
+//    public void initialize() throws IOException {
+//        FileInputStream serviceAccount = new FileInputStream("src/main/resources/donation-21c1e-firebase-adminsdk-euymx-8a49dfd3f5.json");// Firebase Admin SDK JSON 파일 경로
+//
+//        FirebaseOptions options = new FirebaseOptions.Builder()
+//                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                .setDatabaseUrl("https://donation-21c1e-default-rtdb.firebaseio.com") // Firebase Realtime Database URL
+//                .build();
+//
+//        FirebaseApp.initializeApp(options);
+//    }
+//}
