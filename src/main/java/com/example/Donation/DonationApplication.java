92f7f7318bde45dec1a4f91de9606173ecd02340
sangjun19 <sangjun0319@gmail.com>
2023-09-23 15:38:45 +0900

    add donatee info

--- src/main/java/com/example/Donation/DonationApplication.java
+++ src/main/java/com/example/Donation/DonationApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DonationApplication {
 
 	public static void main(String[] args) {
